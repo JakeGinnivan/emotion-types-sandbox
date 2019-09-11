611b9fea76bd33e34d1ab22d75006e59c1966844
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2019-09-11 21:19:50 +0800

    baseline emotion 10

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from '@emotion/styled'
+
+const StyledComponent = styled('div')({
+  width: 100,
+  height: 100,
+  background: 'black'
+})
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <StyledComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
